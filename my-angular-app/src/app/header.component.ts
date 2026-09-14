4ad377956fba9f7982444a159910f72d956e4532
Ridwan2024-P <ahsanrid@gmail.com>
2026-09-14 12:30:17 +0600

    css file add

--- my-angular-app/src/app/header.component.ts
+++ my-angular-app/src/app/header.component.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 @Component({
   selector : 'app-header',
   standalone :true,
-  templateUrl : './header.Component.html'
+  templateUrl : './header.Component.html',
+  styleUrl :  './header.component.css'
 })
 export class HeaderComponent{}
