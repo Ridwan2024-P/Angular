a6ee8e086e5a8fdfa6cda7b66aeeef08dff820e2
Ridwan2024-P <ahsanrid@gmail.com>
2026-09-14 18:12:50 +0600

    User specific Tasks

--- my-angular-app/src/app/tasks/tasks.ts
+++ my-angular-app/src/app/tasks/tasks.ts
@@ -1,6 +1,7 @@
 import { Component,Input } from '@angular/core';
 import { Task } from './task/task';
 
+
 @Component({
   selector: 'app-tasks',
   imports: [Task],
@@ -8,5 +9,35 @@ import { Task } from './task/task';
   styleUrl: './tasks.scss',
 })
 export class Tasks {
- @Input() name!:string;
+  @Input({required :true}) userId!:string;
+ @Input({required:true}) name!:string;
+ tasks= [{
+  id : 't1',
+  userId:'u1',
+  tittle : 'Master Angular',
+  summary : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta repellendus corrupti vel natus excepturi dolore, vitae quis molestiae eaque placeat.',
+  dueDate : '2026-12-12'
+
+ },
+ {
+  id : 't2',
+  userId:'u2',
+  tittle : 'Master Angular',
+  summary : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta repellendus corrupti vel natus excepturi dolore, vitae quis molestiae eaque placeat.',
+  dueDate : '2026-12-12'
+
+ },
+ {
+  id : 't3',
+  userId:'u3',
+  tittle : 'Master Angular',
+  summary : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta repellendus corrupti vel natus excepturi dolore, vitae quis molestiae eaque placeat.',
+  dueDate : '2026-12-12'
+
+ },
+]
+get selectedUserTasks(){
+  return this.tasks.filter((task)=> task.userId === this.userId )
+}
+
 }
