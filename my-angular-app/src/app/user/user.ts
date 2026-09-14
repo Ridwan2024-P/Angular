d80308e7d2f5fb3b9b97e9084e9f97a8445d4d64
Ridwan2024-P <ahsanrid@gmail.com>
2026-09-14 13:37:37 +0600

    Managing State & changing Data

--- my-angular-app/src/app/user/user.ts
+++ my-angular-app/src/app/user/user.ts
@@ -14,6 +14,7 @@ export class User {
     return this.selectedUser.avatar
   }
   onSelectUser(){
-    console.log('Clicked')
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+     this.selectedUser = DUMMY_USERS [randomIndex]
   }
 }
