7e1b217ba16acd76fda1566010a9b41c1d88ec6e
Ridwan2024-P <ahsanrid@gmail.com>
2026-09-15 11:37:03 +0600

    add task button connect

--- my-angular-app/src/app/tasks/tasks.ts
+++ my-angular-app/src/app/tasks/tasks.ts
@@ -1,16 +1,18 @@
 import { Component,Input } from '@angular/core';
 import { Task } from './task/task';
+import { NewTask } from './new-task/new-task';
 
 
 @Component({
   selector: 'app-tasks',
-  imports: [Task],
+  imports: [Task, NewTask],
   templateUrl: './tasks.html',
   styleUrl: './tasks.scss',
 })
 export class Tasks {
   @Input({required :true}) userId!:string;
  @Input({required:true}) name!:string;
+ isAddingTask = false;
  tasks= [{
   id : 't1',
   userId:'u1',
@@ -65,6 +67,9 @@ get selectedUserTasks(){
 }
   onCompleteTask(id:string){
     this.tasks=this.tasks.filter((task)=> task.id !== id)
+  }
+  onStartAddTask(){
+this.isAddingTask = true
   }
 
 }
