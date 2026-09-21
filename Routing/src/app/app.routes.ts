2479027b157b20ee46825a6d973915e501f84d08
Ridwan2024-P <ahsanrid@gmail.com>
2026-09-21 17:47:09 +0600

    Routing Finish

--- Routing/src/app/app.routes.ts
+++ Routing/src/app/app.routes.ts
@@ -2,13 +2,22 @@ import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { SignUp } from './sign-up/sign-up';
 import { App } from './app';
+import { User } from './user/user';
+import { Profile } from './user/profile/profile';
 
 export const routes: Routes = [
     {path:'',component:App},
     {path:'login', component:Login},
     {path:'Sign_up', component:SignUp},
+    {path: '**',redirectTo: 'login'},
+     {
+    path: 'user',
+    component: User,
+    children: [
       {
-    path: '**',
-    redirectTo: 'login'
-  }
+        path: 'profile',
+        component: Profile
+      }
+    ]
+  },
 ];
